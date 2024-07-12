bdc92bdc655d8a34ff7ffa2c0cd141a0d300576a
EllieChoi19981008 <ellie19981008@gmail.com>
2024-07-12 09:35:59 +0900

    project initialization

---
+++ src/main/java/com/kosa/kmt/KmtApplication.java
@@ -0,0 +1,13 @@
+package com.kosa.kmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kosa/kmt/KmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosa.kmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
