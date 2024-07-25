ba73203f363ac9d966f89de75264e32d67f64f0a
onestone <onestone1380@gmail.com>
2024-07-25 17:35:42 +0900

    Feat

--- src/main/java/com/kosa/kmt/MainController.java
+++ src/main/java/com/kosa/kmt/MainController.java
@@ -29,7 +29,7 @@ public class MainController {
 
     @GetMapping("/kommunity/main")
     public String main() {
-        return "main";
+        return "danamusup";
     }
 
     @GetMapping("/login")
